96fba5564ba8cb7d2dc96caa56e0767ab1ec2d2d
Bertille De Almeida <a_bertille@msn.com>
2023-02-14 15:10:28 +0100

    ajout pages contact, carte, reservation

--- includes/header.php
+++ includes/header.php
@@ -6,8 +6,10 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta description="Venez déguster des plats savoureux et gastronomiques dans notre restaurant !">
-  <link rel="stylesheet" href="scss/bootstrap.css">
+
+  <link rel="stylesheet" type="text/css" href="scss/bootstrap.css">
   <link rel="stylesheet" type="text/css" href="scss/style.css">
+
   <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
   <title><?= $titrePrincipal ?? "Quai Antique" ?></title>
